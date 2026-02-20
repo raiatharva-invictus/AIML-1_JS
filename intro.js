7b904e031cf8255b72255ad77897d7aebce9f349
raiatharva-invictus <raiatharva24.in@gmail.com>
2026-02-20 10:30:48 +0530

    done with basic intro

--- intro.js
+++ intro.js
@@ -1,1 +1,12 @@
 console.log("Hello world!");
+// document.write("Hello world");
+
+let val = null;
+let val2 = undefined;
+let val3 = "Hello world";
+let val4 = 1234;
+console.log(val, val2, val3, val4)
+
+const sym1 = Symbol(4);
+const sym2 = Symbol(4);
+console.log(sym1, sym2)
