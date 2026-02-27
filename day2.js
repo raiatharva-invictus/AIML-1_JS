a8856cc26fa14aa9440323b394974b8b855561fa
raiatharva-invictus <raiatharva24.in@gmail.com>
2026-02-27 09:20:36 +0530

    learned more in javascript

--- day2.js
+++ day2.js
@@ -5,24 +5,43 @@ let result = (a == b);
 let result1 = (a >= b);
 let resul2 = (a <= b);
 
-for(let i = 0; i < 10; i++){
-    console.log(i);
-}
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
 
 const obj = {
     "name" : "Atharva",
     "class" : "aiml-1"
 }
-for( key in obj){
-    console.log(key)
-}
+// for( key in obj){
+//     console.log(key)
+// }
 for( let x in obj){
-    console.log(obj[x])
+    console.log(obj[x]) // gets the value of the key
+    // console.log(obj.x) // undefined 
 }
-for( key in obj){
-    console.log(key)
-}
+// for( key in obj){
+//     console.log(key)
+// }
 
-for(const [key, value] of Object.entries(obj)){
-    console.log(`${key} : ${value}`)
+// for(const [key, value] of Object.entries(obj)){
+//     console.log(`${key} : ${value}`)
+// }
+
+let grade = 'A';
+switch(grade){
+    case 'A':
+        console.log("Excellent");
+        break;
+    case 'B':
+        console.log("Good"); 
+        break;
+    case 'C':
+        console.log("Average");
+        break;
+    case 'D':
+        console.log("Poor");
+        break;
+    default:
+        console.log("Invalid grade");
 }
